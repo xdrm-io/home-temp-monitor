a1ea5ae455d8fbc5e9955fa3d5564581ddfa263d
xdrm <xdrm.dev@gmail.com>
2023-11-01 10:58:52 +0100

    feat: create simplistic homepage with calendar range picker

--- station/endpoint/static/main.js
+++ station/endpoint/static/main.js
@@ -1,123 +1,18 @@
-// proxy query parameters to the api
-const params = new URLSearchParams(window.location.search)
-let query = ""
-for( const param of params ){
-    const key   = param[0]
-    const value = param[1]
-    query += `${key}=${value}&`
-}
+const calendar = document.getElementById('calendar');
+const picker = new DateRangePicker(calendar, {
 
-// fetch data from "GET http://192.168.1.10:8000/" in vanilla js
-fetch(`/api/?${query}`).then( (response) => response.json() ).then( (data) => {
+});
 
-    let startingDate = null
-
-    let tempSeries = []
-    let humiditySeries = []
-    let colorIndex = 0;
-    for( const room in data ) {
-        colorIndex = (colorIndex+1) % Highcharts.getOptions().colors.length;
-
-        if( data[room].length > 0 && data[room][0].t < startingDate ) {
-            startingDate = data[room][0].t
-        }
-        tempSeries.push({
-            name: `${room}`,
-            data: data[room].map( (d) => [d.t*1000, d.tavg] ),
-            color: Highcharts.getOptions().colors[colorIndex],
-            zIndex: 1,
-        })
-        tempSeries.push({
-            name: `${room} min/max`,
-            data: data[room].map( (d) => [d.t*1000, d.tmin, d.tmax] ),
-            type: 'arearange',
-            lineWidth: 0,
-            linkedTo: ':previous',
-            color: Highcharts.getOptions().colors[colorIndex],
-            fillOpacity: 0.2,
-            zIndex: 0,
-            marker: { enabled: false }
-        })
-        humiditySeries.push({
-            name: `${room}`,
-            data: data[room].map( (d) => [d.t*1000, d.havg] ),
-            color: Highcharts.getOptions().colors[colorIndex],
-            zIndex: 1,
-        })
-        humiditySeries.push({
-            name: `${room} min/max`,
-            data: data[room].map( (d) => [d.t*1000, d.hmin, d.hmax] ),
-            type: 'arearange',
-            lineWidth: 0,
-            linkedTo: ':previous',
-            color: Highcharts.getOptions().colors[colorIndex],
-            fillOpacity: 0.2,
-            zIndex: 0,
-            marker: { enabled: false }
-        })
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const from = picker.getDates()[0];
+    const to   = picker.getDates()[1];
+    if( !from || !to ){
+        return;
     }
 
 
-    const baseConfig = {
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-        chart: {
-            type: 'line'
-        },
-        xAxis: {
-            type: 'datetime',
-        },
-        plotOptions: {
-            line: {
-                dataLabels: {
-                    enabled: true
-                },
-                enableMouseTracking: true
-            },
-        },
-    }
-
-    Highcharts.chart('temperatures', {
-        ...baseConfig,
-        title: {
-            text: 'Temperature per room'
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            }
-        },
-        tooltip: {
-            crosshairs: true,
-            shared: true,
-            valueSuffix: '°C'
-        },
-        series: tempSeries,
-    });
-
-    Highcharts.chart('humidity', {
-        ...baseConfig,
-        title: {
-            text: 'Humidity per room'
-        },
-        yAxis: {
-            title: {
-                text: 'Humidity (%)'
-            }
-        },
-        tooltip: {
-            crosshairs: true,
-            shared: true,
-            valueSuffix: '%'
-        },
-        series: humiditySeries,
-    });
-
-
-}).catch( (error) => {
-    document.write = `<h1>${error}</h1>`
-})
-
+    // go the the data page with valid query parameters
+    document.location.href = `/data?by=hour&from=${from.getTime()/1000}&to=${to.getTime()/1000}`;
+}, false)
